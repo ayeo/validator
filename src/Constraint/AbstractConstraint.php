d79838baf821b311de0a3f1e3b6e01c208b97f58
Patryk Maryniok <m.patryk1@gmail.com>
2015-06-12 15:00:18 +0200

    Update AbstractConstraint.php
    Change formatting

--- src/Constraint/AbstractConstraint.php
+++ src/Constraint/AbstractConstraint.php
@@ -10,50 +10,52 @@ abstract class AbstractConstraint
 	 */
 	protected $error;
 
-    protected $object;
+	protected $object;
 
-    protected $fieldName;
+	protected $fieldName;
 
 
-    final public function validate()
-    {
-        //check if fieldname and object is set!
-        $this->run($this->getFieldValue());
+	final public function validate()
+	{
+		//check if fieldname and object is set!
+		$this->run($this->getFieldValue());
 
-        return (bool) $this->error;
-    }
+		return (bool)$this->error;
+	}
 
-    public function setObject($object)
-    {
-        if (is_object($object) === false)
-        {
-            //todo throw exception
-        }
+	public function setObject($object)
+	{
+		if (is_object($object) === false)
+		{
+			//todo throw exception
+		}
 
-        $this->object = $object;
-    }
+		$this->object = $object;
+	}
 
-    public function setFieldName($fieldName)
-    {
-        //check if object has property!
-        //event better set it togheter
-        $this->fieldName = $fieldName;
-    }
+	public function setFieldName($fieldName)
+	{
+		//check if object has property!
+		//event better set it togheter
+		$this->fieldName = $fieldName;
+	}
 
-    //fixem!
-    protected function addError($message, $value = null)
-    {
-        $this->error = $this->buildMessage($this->fieldName, $message, $value);
-    }
+	//fixem!
+	protected function addError($message, $value = null)
+	{
+		$this->error = $this->buildMessage($this->fieldName, $message, $value);
+	}
 
-    abstract public function run($value);
-    /**
-     * @return bool
-     */
-    public function isValid()
-    {
-        return !((bool) $this->error);
-    }
+	abstract public function run($value);
+
+	/**
+	 * @return bool
+	 */
+	public function isValid()
+	{
+		return !((bool)$this->error);
+	}
+
 	/**
 	 * @return string
 	 */
@@ -75,42 +77,42 @@ abstract class AbstractConstraint
 		return sprintf($messagePattern, $fieldName, $message, $value);
 	}
 
-    protected function getFieldValue($fieldName = null)
-    {
-        if (is_null($fieldName))
-        {
-            $fieldName = $this->fieldName;
-        }
+	protected function getFieldValue($fieldName = null)
+	{
+		if (is_null($fieldName))
+		{
+			$fieldName = $this->fieldName;
+		}
 
-        $object = $this->object;
+		$object = $this->object;
 
-        $reflection = new ReflectionClass(get_class($object));
+		$reflection = new ReflectionClass(get_class($object));
 
-        try
-        {
-            $property = $reflection->getProperty($fieldName);
-        }
-        catch (\Exception $e)
-        {
-            $property = null;
-        }
+		try
+		{
+			$property = $reflection->getProperty($fieldName);
+		}
+		catch (\Exception $e)
+		{
+			$property = null;
+		}
 
 
-        $methodName = 'get'.ucfirst($fieldName);
+		$methodName = 'get' . ucfirst($fieldName);
 
-        if ($property && $property->isPublic())
-        {
-            $value = $property->getValue($object);
-        }
-        else if ($reflection->hasMethod($methodName))
-        {
-            $value = call_user_func(array($object, $methodName));
-        }
-        else
-        {
-            throw new \Exception('Object has not property nor method: '. $fieldName);
-        }
+		if ($property && $property->isPublic())
+		{
+			$value = $property->getValue($object);
+		}
+		else if ($reflection->hasMethod($methodName))
+		{
+			$value = call_user_func(array($object, $methodName));
+		}
+		else
+		{
+			throw new \Exception('Object has not property nor method: ' . $fieldName);
+		}
 
-        return $value;
-    }
+		return $value;
+	}
 }
